87ab7e634ff3eaa1572df7c5e497a0edfdfa8140
Joakim Nilsson <nijoakim@gmail.com>
2016-01-13 20:51:30 +0100

    Initial python server

---
+++ engine/server.py
@@ -0,0 +1,34 @@
+# Copyright 2014-2015 Joakim Nilsson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import socket
+
+TCP_IP      = '127.0.0.1'
+TCP_PORT    = 3000
+BUFFER_SIZE = 20 # Normally 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print('Connection address:', addr)
+while True:
+	data = conn.recv(BUFFER_SIZE)
+	if not data: break
+	print("received data:", data)
+	conn.send(data) # echo whatever comes in
+conn.close()
