c615409c3b3a07d6f7c83d44917de2be39827a9e
Joakim Nilsson <nijoakim@gmail.com>
2026-04-16 21:09:42 +0200

    switch license to Affero GPL3.

--- engine/chakes/engine/game_engine.py
+++ engine/chakes/engine/game_engine.py
@@ -3,17 +3,18 @@
 # This file is part of Chakes
 
 # Chakes is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
 # Chakes is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with Chakes.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with Chakes.  If not, see <https://www.gnu.org/licenses/>.
+
 
 from __future__ import annotations
 
